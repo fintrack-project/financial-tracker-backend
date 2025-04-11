e5342db26a29104bf56d8465b04e9802ec0f1f4b
johnkim1216 <johnmjkim1216@gmail.com>
2025-04-11 17:36:06 +1000

    establish web socket

--- src/main/java/com/fintrack/repository/MarketDataRepository.java
+++ src/main/java/com/fintrack/repository/MarketDataRepository.java
@@ -10,6 +10,6 @@ import java.util.*;
 
 @Repository
 public interface MarketDataRepository extends JpaRepository<MarketData, UUID> {
-    @Query("SELECT m.id, m.symbol, m.price, m.percent_change, m.timestamp, m.asset_name, m.price_unit FROM market_data m WHERE m.asset_name IN (:assetNames)")
+    @Query(value="SELECT m.id, m.symbol, m.price, m.percent_change, m.timestamp, m.asset_name, m.price_unit FROM market_data m WHERE m.asset_name IN (:assetNames)", nativeQuery = true)
     List<MarketData> findMarketDataByAssetNames(@Param("assetNames") List<String> assetNames);
 }
