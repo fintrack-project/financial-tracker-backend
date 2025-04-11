996209dc1bc225f7e74bec060c1bbb19bd4a77d8
johnkim1216 <johnmjkim1216@gmail.com>
2025-04-11 17:34:48 +1000

    establish web socket

--- src/main/java/com/fintrack/config/WebSocketConfig.java
+++ src/main/java/com/fintrack/config/WebSocketConfig.java
@@ -21,6 +21,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
-} {
-  
 }
