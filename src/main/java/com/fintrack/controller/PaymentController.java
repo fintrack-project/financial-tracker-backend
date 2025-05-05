ce2984e1559d381186595c0b2b8edeaeff310972
johnkim1216 <johnmjkim1216@gmail.com>
2025-05-05 19:34:11 +1000

    add payment

--- src/main/java/com/fintrack/controller/PaymentController.java
+++ src/main/java/com/fintrack/controller/PaymentController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/payments")
+@RequestMapping("/api/user/payments")
 public class PaymentController {
 
     private final PaymentService paymentService;
