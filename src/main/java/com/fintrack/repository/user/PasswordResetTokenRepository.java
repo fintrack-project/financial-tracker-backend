6ce73f436d7921c956287153f6dfa935de6cec9e
johnkim1216 <johnmjkim1216@gmail.com>
2025-05-09 20:54:59 +1000

    resset password input

---
+++ src/main/java/com/fintrack/repository/user/PasswordResetTokenRepository.java
@@ -0,0 +1,13 @@
+package com.fintrack.repository.user;
+
+import com.fintrack.model.user.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    Optional<PasswordResetToken> findByToken(String token);
+    Optional<PasswordResetToken> findByUserIdAndUsedFalse(String userId);
+} 
